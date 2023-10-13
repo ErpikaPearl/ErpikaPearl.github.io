ff3bd2ebf6a2d6c1bd67a075b33b2885ea3aa420
ErpikaPearl <112347457+ErpikaPearl@users.noreply.github.com>
2023-10-13 12:17:06 -0600

    tried to refactor code, it needs a LOT more work

--- 02-arrays_project/sketch.js
+++ 02-arrays_project/sketch.js
@@ -6,41 +6,8 @@
 // - describe what you did to take this project "above and beyond"
 
 //  Declaring objects
-const partyMemberZero = {
-  hP: 50,
-  mP: 25,
-  attack: 50,
-  defence: 20,
-  hitRate: 5,
-  abiliies: ["one", "two", "three", "four"]
-};
 
-const partyMemberOne = {
-  hP: 50,
-  mP: 25,
-  attack: 50,
-  defence: 20,
-  hitRate: 5,
-  abiliies: ["one", "two", "three", "four"]
-};
 
-const partyMemberTwo = {
-  hP: 50,
-  mP: 25,
-  attack: 50,
-  defence: 20,
-  hitRate: 5,
-  abiliies: ["one", "two", "three", "four"]
-};
-
-const partyMemberThree = {
-  hP: 50,
-  mP: 25,
-  attack: 50,
-  defence: 20,
-  hitRate: 5,
-  abiliies: ["one", "two", "three", "four"]
-};
 
 //  Declaring variables
 let borderSize = 10; // Universal border for everything
@@ -52,10 +19,25 @@ let backgroundY = 0;
 let backgroundWidth = 0;
 let backgroundHeight = 0;
 
-// let boxWidth = 0;
-// let boxHeight = 0;
-// let boxYLocation = 0;
-let boxColours = ["red", "red",  "red", "red"];
+
+const boxParent = {
+  x: borderSize*4,
+  y: 0,
+  w: 0,
+  h: 0,
+  colour: "white",
+};
+
+const boxChild0 = {
+  x: 0,
+  y: 0,
+  w: 0,
+  h: 0,
+  colour: "red",
+};
+let boxChild1 = {};
+let boxChild2 = {};
+let boxChild3 = {};
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -63,19 +45,35 @@ function setup() {
   //  Adding to variables
   midLine = width/2;
 
-  backgroundX = width/2;
-  backgroundY = height/1.3;
-  backgroundWidth = width/1.4;
-  backgroundHeight = height/3.5;
+  boxParent.y = height/1.5;
+  boxParent.w = width - borderSize*8;
+  boxParent.h = height/3 - borderSize*2;
+
+  boxChild0.x = boxParent.x + borderSize;
+  boxChild0.y = boxParent.y + borderSize;
+  boxChild0.w = boxParent.w/2 - borderSize*2;
+  boxChild0.h = boxParent.h/2 - borderSize*2;
+  boxChild1 = structuredClone(boxChild0);
+  boxChild2 = structuredClone(boxChild0);
+  boxChild3 = structuredClone(boxChild0);
+
+  boxChild1.x = boxChild0.x + borderSize;
+  boxChild1.y = boxChild0.y + borderSize;
+  boxChild2.x = boxChild0.x;
+  boxChild2.y = boxChild0.y + borderSize + boxChild0.h;
+  boxChild3.x = boxChild2.x + borderSize + boxChild2.w;
+  boxChild3.y = boxChild2.y + borderSize + boxChild0.h;
+
 }
 
 function draw() {
   background(220);
-  drawBackground();
-  drawBattleScreen();
-  drawChoiceScreen()
-
-  console.log(screenType);
+  fill(boxParent.colour);
+  rect(boxParent.x, boxParent.y, boxParent.w, boxParent.h);
+  rect(boxChild0.x, boxChild0.y, boxChild0.w, boxChild0.h);
+  rect(boxChild1.x, boxChild1.y, boxChild1.w, boxChild1.h);
+  rect(boxChild2.x, boxChild2.y, boxChild2.w, boxChild2.h);
+  rect(boxChild3.x, boxChild3.y, boxChild3.w, boxChild3.h);
 }
 
 function whileMousePressed(x, y, w, h){
@@ -84,73 +82,64 @@ function whileMousePressed(x, y, w, h){
   return mouseX > x && mouseX < x + w && mouseY > y && mouseY < y + h && mouseIsPressed;
 }
 
+// function drawBattleScreen(){
+//   if (screenType === "battle"){
+
+//     //  Determining size of boxes & where to place
+//     let boxWidth = backgroundWidth/2.5;
+//     let boxHeight = backgroundHeight/2.5;
+//     let boxYLocation = backgroundY;
+
+//     //  Left Boxes
+//     fill(boxColours[0]);
+//     rect(midLine - boxWidth - borderSize/2, boxYLocation - boxHeight - borderSize, boxWidth, boxHeight);
+//     fill(boxColours[1]);
+//     rect(midLine - boxWidth - borderSize/2, boxYLocation, boxWidth, boxHeight);
+//     //  Right  boxes
+//     fill(boxColours[2]);
+//     rect(midLine + borderSize/2, boxYLocation - boxHeight- borderSize, boxWidth, boxHeight);
+//     fill(boxColours[3]);
+//     rect(midLine + borderSize/2, boxYLocation, boxWidth, boxHeight);
+
+//     // if boxes clicked, turn black
+//     if  (whileMousePressed(midLine - boxWidth - borderSize/2, boxYLocation - boxHeight - borderSize, boxWidth, boxHeight)){
+//       boxColours[0] = "black";
+//       screenType = "attack";
+//     }
+//     else if  (whileMousePressed(midLine - boxWidth - borderSize/2, boxYLocation, boxWidth, boxHeight)){
+//       boxColours[1] = "black";
+//       screenType = "run";
+//     }
+//     else if  (whileMousePressed(midLine + borderSize/2, boxYLocation - boxHeight- borderSize, boxWidth, boxHeight)){
+//       boxColours[2] = "black";
+//       screenType = "ability";
+//     }
+//     else if  (whileMousePressed(midLine + borderSize/2, boxYLocation, boxWidth, boxHeight)){
+//       boxColours[3] = "black";
+//       screenType = "heal";
+//     }
+//     else{
+//       boxColours[0] = "red";
+//       boxColours[1] = "red";
+//       boxColours[2] = "red";
+//       boxColours[3] = "red";
+//     }
+//   }  
+// }
 
 
 
+// function drawChoiceScreen(){
+//   if (screenType === "ability"){
+//     let boxWidth = backgroundWidth;
+//     let boxHeight = backgroundHeight;
+//     let boxYLocation = backgroundY;
 
-function drawBattleScreen(){
-  if (screenType === "battle"){
+//     fill("green");
+//     rect(midLine - background, boxYLocation - boxHeight - borderSize*3, boxWidth, boxHeight); //  THING
+//   }
 
-    //  Determining size of boxes & where to place
-    let boxWidth = backgroundWidth/2.5;
-    let boxHeight = backgroundHeight/2.5;
-    let boxYLocation = backgroundY;
-
-    //  Left Boxes
-    fill(boxColours[0]);
-    rect(midLine - boxWidth - borderSize/2, boxYLocation - boxHeight - borderSize, boxWidth, boxHeight);
-    fill(boxColours[1]);
-    rect(midLine - boxWidth - borderSize/2, boxYLocation, boxWidth, boxHeight);
-    //  Right  boxes
-    fill(boxColours[2]);
-    rect(midLine + borderSize/2, boxYLocation - boxHeight- borderSize, boxWidth, boxHeight);
-    fill(boxColours[3]);
-    rect(midLine + borderSize/2, boxYLocation, boxWidth, boxHeight);
-
-    // if boxes clicked, turn black
-    if  (whileMousePressed(midLine - boxWidth - borderSize/2, boxYLocation - boxHeight - borderSize, boxWidth, boxHeight)){
-      boxColours[0] = "black";
-      screenType = "attack";
-    }
-    else if  (whileMousePressed(midLine - boxWidth - borderSize/2, boxYLocation, boxWidth, boxHeight)){
-      boxColours[1] = "black";
-      screenType = "run";
-    }
-    else if  (whileMousePressed(midLine + borderSize/2, boxYLocation - boxHeight- borderSize, boxWidth, boxHeight)){
-      boxColours[2] = "black";
-      screenType = "ability";
-    }
-    else if  (whileMousePressed(midLine + borderSize/2, boxYLocation, boxWidth, boxHeight)){
-      boxColours[3] = "black";
-      screenType = "heal";
-    }
-    else{
-      boxColours[0] = "red";
-      boxColours[1] = "red";
-      boxColours[2] = "red";
-      boxColours[3] = "red";
-    }
-  }  
-}
-
-function drawBackground(){
-  rectMode(CENTER);
-  fill("white");
-  rect(backgroundX, backgroundY, backgroundWidth, backgroundHeight);
-  rectMode(CORNER);
-}
-
-function drawChoiceScreen(){
-  if (screenType === "ability"){
-    let boxWidth = backgroundWidth;
-    let boxHeight = backgroundHeight;
-    let boxYLocation = backgroundY;
-
-    fill("green");
-    rect(midLine - background, boxYLocation - boxHeight - borderSize*3, boxWidth, boxHeight); //  THINGG
-  }
-
-}
+// }
 
 
 
