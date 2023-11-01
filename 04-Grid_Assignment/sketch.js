a69dcaeb151b86c676d621d35eed4dc06ab04553
ErpikaPearl <112347457+ErpikaPearl@users.noreply.github.com>
2023-11-01 12:18:18 -0600

    once again tried fixing floodfill. Started  adding pallate

--- 04-Grid_Assignment/sketch.js
+++ 04-Grid_Assignment/sketch.js
@@ -5,28 +5,58 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
+
+// number ker
+//  0 = open space
+//  1 = obstacle (tree)
+//  4 = obstical (water)
+//  5 = background 
+
+
 let gridLayerOne;
 let gridLayerTwo;
 let cellSize;
+let screenMode;
+let drawState = 1;
+let border;
 const GRID_SIZE = 50;
+
+let colourChoiceBox = {
+  x: 0,
+  y: 0,
+  w: 0,
+  h: 0,
+};
+
 
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   if (height < width){
     cellSize = height/GRID_SIZE;
+    screenMode = "landscape";
   }
   else {
     cellSize = width/GRID_SIZE;
+    screenMode = "portrait";
   }
   gridLayerOne = generateEmptyGrid(GRID_SIZE, GRID_SIZE);
   gridLayerTwo = generateEmptyGrid(GRID_SIZE, GRID_SIZE);
+
+  border = cellSize;
+
+  colourChoiceBox.x = border;
+  colourChoiceBox.y = GRID_SIZE * cellSize + border ;
+  colourChoiceBox.w = (GRID_SIZE - 2) / 4;
+  colourChoiceBox.h = 60;
+
 }
 
 function draw() {
   background(220);
   // displayGrid(gridLayerOne);
   displayGrid(gridLayerTwo);
+  chooseDrawState();
 }
 
 function mousePressed(){
@@ -35,6 +65,33 @@ function mousePressed(){
   }
   else if (mouseButton === CENTER){
     floodFill(floor(mouseX/cellSize), floor(mouseY/cellSize), gridLayerTwo);
+  }
+}
+
+function keyTyped(){
+  if (key === "e"){
+    gridLayerTwo = generateEmptyGrid(GRID_SIZE, GRID_SIZE);
+  }
+
+  // else if (key === "d"){
+  //   drawShape();
+  // }
+}
+
+function floodFill(x, y, grid){
+  if (grid[y][x] !== 1){
+    grid[y][x] = 1;
+    for (let cols = y - 1; cols < y + 1; y++){
+      for (let rows = x - 1; rows < x + 1; x++){
+        //  edge case check
+        if (rows >= 0 && rows <= GRID_SIZE && cols >= 0 && cols <= GRID_SIZE && grid[cols][rows] !== 1){
+          floodFill(rows, cols, grid);
+        }
+        else{
+          break;
+        }
+      }
+    }
   }
 }
 
@@ -49,33 +106,8 @@ function ifClicked(xLoc, yLoc, grid){
   }
 }
 
-function keyTyped(){
-  // if (key === "r"){
-  //   grid = generateObstacles(GRID_SIZE, GRID_SIZE);
-  // }
-  if (key === "e"){
-    gridLayerOne = generateEmptyGrid(GRID_SIZE, GRID_SIZE);
-  }
-  else if (key === "d"){
-    drawShape();
-  }
-}
-
-function floodFill(x, y, grid){
-  if (grid[y][x] !== 1){
-    grid[y][x] === 1;
-    // for (let cols = y - 1; cols < y + 1; y++){
-    //   for (let rows = x - 1; rows < x + 1; x++){
-    //     //  edge case check
-    //     if (rows >= 0 && rows <= GRID_SIZE && cols >= 0 && cols <= GRID_SIZE){
-    //       floodFill(rows, cols, grid);
-    //     }
-    //     else{
-    //       break;
-    //     }
-    //   }
-    // }
-  }
+function chooseDrawState(){
+  rect(colourChoiceBox.x, colourChoiceBox.y, colourChoiceBox.w, colourChoiceBox.h);
 }
 
 function generateEmptyGrid(cols, rows){
@@ -89,15 +121,15 @@ function generateEmptyGrid(cols, rows){
   return randomArray;
 }
 
-function drawShape(grid, orginX, orginY){
-  let averageSize = 3;
-  //  place a marker at orgin
-  //  place 1-2 pixel at average size on each side
-  //    if one pixel is on left side, dont on right (same for top and bottom)
-  //  place 1 pixel on each corner
-  //    one pixel out when in bwteen long pxiels, one innside when beside it
-  //  CONNECT IT??????
-}
+// function drawShape(grid, orginX, orginY){
+//   let averageSize = 3;
+//   //  place a marker at orgin
+//   //  place 1-2 pixel at average size on each side
+//   //    if one pixel is on left side, dont on right (same for top and bottom)
+//   //  place 1 pixel on each corner
+//   //    one pixel out when in bwteen long pxiels, one innside when beside it
+//   //  CONNECT IT??????
+// }
 
 
 
@@ -111,22 +143,6 @@ function toggleCell(x, y, grid){
       grid[y][x] = 0;
     }
   }
-}
-
-function generateObstacles(cols, rows){
-  let randomArray = [];
-  for (let y = 0; y < cols; y++){
-    randomArray.push([]);
-    for(let x = 0; x < rows; x++){
-      if (random(100) < 50) {
-        randomArray[y].push(0);
-      }
-      else{
-        randomArray[y].push(1);
-      }
-    }
-  }
-  return randomArray;
 }
 
 function displayGrid(grid){
